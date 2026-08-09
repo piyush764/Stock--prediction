a1c31ed21e367ac7c0a10fc96de072e6264eb7fe
piyush bohra <pbohra134@gmail.com>
2026-08-09 17:23:09 +0530

    Writing Train Test

--- code.py
+++ code.py
@@ -1,6 +1,22 @@
 #%%
 import yfinance as yf
 #%%
-data = yf.download('ADANIPOWER.NS', start='2023-01-01', end='2024-01-01')
+data = yf.download('ADANIPOWER.NS', start='2020-01-01', end='2025-01-01')
 print (data.head())
+
+# %%
+print(data.shape)
+#%%
+print(data.head())
+
+# %%
+data['Prev_Close'] = data['Close'].shift(1)
+#%%
+data = data.dropna()
+#%%
+Y = data['Close']
+X = data[['Prev_Close']]
+#%%
+print(X.head())
+print(Y.head())
 # %%
