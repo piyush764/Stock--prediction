949254655b611d6d576ff3b4be1d912a8e0e06ad
piyush bohra <pbohra134@gmail.com>
2026-08-12 14:24:26 +0530

    errors

--- flask_app/flask1.py
+++ flask_app/flask1.py
@@ -8,6 +8,20 @@ model = pickle.load(open('stock_model.pkl','rb'))
 def home():
     return render_template('stock_form.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [
+        float(request.form['Prev_Close']),
+        float(request.form['MA5']),
+        float(request.form['MA10']),
+        float(request.form['Pct_Change'])
+    ]
+    prediction = model.predict([features])
+    print(prediction)
+    print(type(prediction))
+    print(prediction.shape)
+    return {"predicted_price": float(prediction[0])}
+
 if __name__ == "__main__":
     app.run(debug=True)
 
